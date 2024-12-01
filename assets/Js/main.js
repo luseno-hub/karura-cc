c9b9bfa4a2ccd72b7731ec27110ddedb13e22dcf
m_luseno <luseno@outlook.com>
2024-12-01 18:55:11 +0300

    city on a hill ui

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -69,6 +69,23 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// ---------------> Page scroll animatins <------------
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
 // ---------------> Progress Bar <----------------
 function setProgress(element, percent) {
   const radius = 54; // Radius of the circle
